9285b19f4d696088e5c6f586dd3b1194705f6502
nematatu <nematatu1003@gmail.com>
2024-02-20 21:56:33 +0900

    Revert "docker環境"
    This reverts commit d8a20c67b8455405feb70b2426099dadd35ede76.

--- streamlit_test/app/memoback/database.py
+++
@@ -1,9 +0,0 @@
-#DBの設定
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
-
-DB_URL='mysql://root@db:3306/demo?charset=utf8'
-
-engine=create_engine(DB_URL,echo=True)
-Sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base=declarative_base()
